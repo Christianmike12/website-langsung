eeee42f66d6d474583fa2e99c5778a4965b93499
Christianmike12 <christianmikhael93@gmail.com>
2025-10-14 10:10:08 +0700

    hapus index.css

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+  section.scrollIntoView({ behavior: "smooth" });
+}
+
+const sections = document.querySelectorAll(".section");
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    const position = section.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight / 1.3;
+    if (position < screenPosition) {
+      section.classList.add("visible");
+    }
+  });
+});
